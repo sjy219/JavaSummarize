1888a09dc5498e934b6a4c03b68312aa7b665fac
332754474@qq.com <332754474@qq.com>
2022-09-28 13:32:33 +0800

    changes

--- io/src/colletionio/CollectFile.java
+++ io/src/colletionio/CollectFile.java
@@ -33,9 +33,9 @@ public class CollectFile {
         bw.close();
     }
 
-            //------------文件到集合------------\\
+    //------------文件到集合------------\\
     @Test
-    public void filetocoll() throws Exception{
+    public void filetocoll() throws Exception {
         //创建ArrayList集合对象
         ArrayList<String> arrayList = new ArrayList<>();
 
@@ -44,7 +44,7 @@ public class CollectFile {
 
         //把文件中的数据写入集合
         String line;
-        while ((line=br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
             arrayList.add(line);
         }
         //遍历集合，将集合数据打印到控制台
@@ -56,28 +56,17 @@ public class CollectFile {
         br.close();
 
     }
+
     //----------------集合到文件改进版-----------------\\
     @Test
-    public void colToFilePro() throws Exception{
+    public void colToFilePro() throws Exception {
         //创建ArrayList集合
         ArrayList<Students> arrayList = new ArrayList<>();
 
         //创建学生对象，并赋值
-        Students st1 = new Students();
-        st1.setId(10);
-        st1.setName("张三");
-        st1.setAge(20);
-        st1.setAddress("Big Street");
-        Students st2 = new Students();
-        st2.setId(11);
-        st2.setName("张三si");
-        st2.setAge(20);
-        st2.setAddress("Big Street");
-        Students st3 = new Students();
-        st3.setId(12);
-        st3.setName("张si");
-        st3.setAge(22);
-        st3.setAddress("Big Street");
+        Students st1 = new Students(10, "zhangsan", 20, "hebeishen");
+        Students st2 = new Students(11, "lisi", 20, "hebeishen");
+        Students st3 = new Students(12, "wangwu", 20, "hebeishen");
 
         //把学生对象放入集合中去
         arrayList.add(st1);
@@ -103,5 +92,28 @@ public class CollectFile {
         //释放资源
         bw.close();
     }
+
+    //--------------文件到集合改进版------------------
+        //把文本文件中的数据读取到集合中，并遍历集合，
+        //要求文件中没一行数据是学生对象的成员变量值
+    @Test
+    public void fileToColl() throws Exception {
+        //创建ArrayList集合
+        ArrayList<Students> arrayList = new ArrayList<>();
+
+        //创建文件输入流对象
+        BufferedReader br = new BufferedReader(new FileReader("src/colletionio/newfile.txt"));
+
+        //读写数据，把文件流中的数据写入集合中
+        String line;
+        while ((line=br.readLine()) != null) {
+            String[] split = line.split(",");
+            Students st = new Students(Integer.parseInt(split[0]), split[1], Integer.parseInt(split[2]), split[3]);
+            arrayList.add(st);
+        }
+        //释放资源
+        br.close();
+    }
+
 
 }
